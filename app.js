8533ad112e8cc922097c6ac3502e49d73de04544
saanaa <khiarisanaaa@gmail.com>
2017-05-24 21:48:15 +0100

    ok

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var jsonVersFrontend;
 
 app.post("/travishook", function (req, res) {
 
-  console.log("req.body : "+req);
+  console.log("req.body : "+req.body.status_message);
     // var buildstatus = req.body.status_message;
     // var dateCommit = req.body.committed_at;
     // var user = req.body.author_name;
