4c98ea76ed96cb7e2654daeeec4aec5bf6b47a4f
saanaa <khiarisanaaa@gmail.com>
2017-05-24 17:20:55 +0100

    ok

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
-var port=process.env.PORT;
+var port = process.env.PORT;
 var express = require('express');
 var bodyParser = require('body-parser');
-var app = express();  
+var app = express();
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true,type: application/x-www-form-urlencoded}));
 
 app.get("/", function (req, res) {
- res.send("ok");
+    res.send("ok");
 })
 var jsonVersFrontend;
 
 
 
 app.post("/travishook", function (req, res) {
-  
-  var buildstatus =req.body.status_message;
-  var dateCommit=req.body.committed_at;
-  var user=req.body.committer_name;
-  var repoName=req.body.repository.name;
-console.log("hook is hereeeee !!!! ")
-  jsonVersFrontend="{ \"buildstatus\": \""+buildstatus+"\",\"dateCommit\": \""+dateCommit+"\",\"user\": \""+user+"\",\"repoName\": \""+repoName+"\"}";
+
+    var buildstatus = req.body.status_message;
+    var dateCommit = req.body.committed_at;
+    var user = req.body.committer_name;
+    var repoName = req.body.repository.name;
+    console.log("hook is hereeeee !!!! ");
+    jsonVersFrontend = "{ \"buildstatus\": \"" + buildstatus + "\",\"dateCommit\": \"" + dateCommit + "\",\"user\": \"" + user + "\",\"repoName\": \"" + repoName + "\"}";
 })
 
 
 app.get("/travishook", function (req, res) {
 
 
-res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
- var jsonTosend=JSON.parse(jsonVersFrontend);
-     return res.status(201).send(jsonTosend);
+    var jsonTosend = JSON.parse(jsonVersFrontend);
+    return res.status(201).send(jsonTosend);
 
 })
 
